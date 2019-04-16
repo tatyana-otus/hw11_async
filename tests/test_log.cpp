0400a63dfd449453d2db620c6cb569e3d2715f1d
tatyana-otus <36768428+tatyana-otus@users.noreply.github.com>
2019-04-16 20:34:28 +0300

    Update test_log.cpp

--- tests/test_log.cpp
+++ tests/test_log.cpp
@@ -99,7 +99,7 @@ BOOST_AUTO_TEST_SUITE(test_suite_delete)
 
 BOOST_AUTO_TEST_CASE(remove_test_files)
 {
-    std::system("rm -f bulk*.log");
+   // std::system("rm -f bulk*.log");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
